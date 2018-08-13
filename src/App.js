63c9dcd9939df787539ae5e1477da81124f1e79d
devin.chenyang <d3vin.chen@gmail.com>
2018-08-14 00:01:26 +0800

    Parent render will lose data in child form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
+import debugModule from "debug";
 
 import logo from "./logo.svg";
 import "./App.css";
 import EditForm from "./EditForm";
 
+const debug = debugModule("antd-form-demos:App");
+
 class App extends Component {
+  state = {
+    parent: ''
+  }
+  handleChange = event => this.setState({ parent: event.target.value })
   render() {
+    debug('render()', this.props)
     return (
       <div className="App">
         <header className="App-header">
@@ -15,6 +23,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <input value={this.state.parent} onChange={this.handleChange} />
         <EditForm />
       </div>
     );
--- src/EditForm.jsx
+++ src/EditForm.jsx
@@ -1,40 +1,43 @@
-import React from "react";
-import { Form, Input } from "antd";
-import debugModule from "debug";
+import React from 'react'
+import { Form, Input } from 'antd'
+import debugModule from 'debug'
 
-const debug = debugModule("antd-form-demos:EditForm");
-const FormItem = Form.Item;
+const debug = debugModule('antd-form-demos:EditForm')
+const FormItem = Form.Item
 
-class EditForm extends Component {
-  render() {
-    debug("render()", this.props);
-    const { form } = this.props;
-    const { getFieldDecorator } = form;
+class EditForm extends React.Component {
+  render () {
+    debug('render()', this.props)
+    const { form } = this.props
+    const { getFieldDecorator } = form
     return (
-      <Form layout="vertical">
-        <FormItem label="foo">
-          {getFieldDecorator("foo", {})(<Input />)}
+      <Form layout='vertical'>
+        <FormItem label='foo'>
+          {getFieldDecorator('foo', {})(<Input />)}
         </FormItem>
-        <FormItem label="bar">
-          {getFieldDecorator("bar", {})(<Input />)}
+        <FormItem label='bar'>
+          {getFieldDecorator('bar', {})(<Input />)}
+        </FormItem>
+        <FormItem label='zzz'>
+          {getFieldDecorator('zzz', {})(<Input />)}
         </FormItem>
       </Form>
-    );
+    )
   }
 }
 
 const WrappedEditForm = Form.create({
-  mapPropsToFields(props) {
-    debug("mapPropsToFields(props=?)", props);
+  mapPropsToFields (props) {
+    debug('mapPropsToFields(props=?)', props)
     return {
       foo: Form.createFormField({
-        value: "foo"
+        value: 'foo'
       }),
       bar: Form.createFormField({
-        value: ""
+        value: ''
       })
-    };
+    }
   }
-})(EditForm);
+})(EditForm)
 
-export default WrappedEditForm;
+export default WrappedEditForm
